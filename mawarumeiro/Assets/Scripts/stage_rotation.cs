6e18ea0b6e2725d1ca527ca270aa3b3cf35e493d
himajinto2828 <tokyo.hal.ths70632@gmail.com>
2020-03-04 15:40:40 +0900

    /////////////////////////////////////////////////////////////////////////////////////////// // プリプロの制作状況を反映 // ./Scenes/test .unity // ./Scripts/stage_rotation.cs // ./Texture/test.png // ./Texture/test2.png ///////////////////////////////////////////////////////////////////////////////////////////

---
+++ mawarumeiro/Assets/Scripts/stage_rotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stage_rotation : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Rotate(0.0f, 0.0f, 1.0f);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Rotate(0.0f, 0.0f, -1.0f);
+        }
+
+
+    }
+}
