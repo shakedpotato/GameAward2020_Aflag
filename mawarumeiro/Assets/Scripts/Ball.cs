1ca17e601269a369875c07b273d21bb171f90059
himajinto2828 <tokyo.hal.ths70632@gmail.com>
2020-04-01 17:55:49 +0900

    Ballにリセット機能を実装。
    ///////////////////////////////////////////////////////////////////////////////////////////
    // 追加ファイル
    // Ball.prefab
    // Ball.prefab.meta
    // Ball.cs
    // Ball.cs.meta
    ///////////////////////////////////////////////////////////////////////////////////////////

---
+++ mawarumeiro/Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour
+{
+    public string scene_name;
+
+    Transform m_transform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_transform = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_transform.position.y <= -5.0)
+        {
+            SceneManager.LoadScene(scene_name);
+        }
+
+    }
+
+
+}
