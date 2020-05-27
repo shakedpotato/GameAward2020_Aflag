7f1d601965a55ae09484edaa126c32a75b8681ba
shakedpotato <shakedpotato@gmail.com>
2020-05-27 16:10:19 +0900

    タイトルシーンにステージセレクトシーンへ飛ぶボタンを追加した。 タイトルシーンのボタンのスクリプト名をTitleScene_XXXXXBehaviour.csで統一することにした。 ファイル名の変更: StartButtonBehaviour.cs -> TitleScene_StartButtonBehaviour.cs

---
+++ Assets/Scripts/TitleScene/TitleScene_StageSelectButtonBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleScene_StageSelectButtonBehaviour : MonoBehaviour
+{
+    private string stageSelectSceneName = "SelectStageScene";
+    // Start is called before the first frame update
+    public void OnClick()
+    {
+        Debug.Log("StageSelectButton クリック。");
+
+        // ボタンの色を変える
+        gameObject.GetComponent<Image>().color = Color.cyan;
+
+        // ゲームシーンの非同期ロード開始    
+        Debug.Log("ゲームシーンの非同期読込開始。");
+        SceneManager.LoadSceneAsync(stageSelectSceneName);
+    }
+}
