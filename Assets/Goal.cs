81df0c36b5f5a87e94bfc036b85f29b497fd6133
shakedpotato <shakedpotato@gmail.com>
2020-06-04 11:33:34 +0900

    ゴールオブジェクトのPrefabを追加した。

---
+++ Assets/Goal.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ゴールエリアの画像サイズをColliderの領域に合わせて調整する。
+        CircleCollider2D ccol = GetComponent<CircleCollider2D>();
+        Transform areaImageTransform = gameObject.transform.Find("Image");
+        float scale = ccol.radius * 2.0f; // 半径1.0f == スケール 1.0f
+        areaImageTransform.localScale = new Vector3(scale, scale, scale);
+        // ColliderはあらかじめPrefabに追加しておく。
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Ball")
+        {
+            // ボールが領域に入ったらクリアイベントを呼ぶ。
+            Debug.Log("OnTriggerEnter2D: other is " + collision.gameObject.name);
+
+        }
+    }
+}
