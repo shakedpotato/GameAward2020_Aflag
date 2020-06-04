c239eac87cc2eea3eccfd6659fb4481d17e5da1e
shakedpotato <shakedpotato@gmail.com>
2020-06-04 12:02:52 +0900

    ゴール後に生成されるイメージオブジェクトを追加した。 とりあえずで、ゴール後の演出を追加した。

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 
+
 public class Goal : MonoBehaviour
 {
+    // クリア後に生成するオブジェクト
+    [SerializeField]
+    private GameObject objectAfterClear;
+
+    // ゴールしているかどうか
+    bool isCleared = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +22,6 @@ public class Goal : MonoBehaviour
         float scale = ccol.radius * 2.0f; // 半径1.0f == スケール 1.0f
         areaImageTransform.localScale = new Vector3(scale, scale, scale);
         // ColliderはあらかじめPrefabに追加しておく。
-
     }
 
     // Update is called once per frame
@@ -25,11 +32,22 @@ public class Goal : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.name == "Ball")
+        if (isCleared)
+            return; // クリア後は呼ばない。
+
+        if (collision.gameObject.name == "Ball")
         {
             // ボールが領域に入ったらクリアイベントを呼ぶ。
             Debug.Log("OnTriggerEnter2D: other is " + collision.gameObject.name);
+            
+            // クリア後のイベント
+            Instantiate(objectAfterClear);
 
+            // スポナーの排水を止める。
+
+
+            // おわり
+            isCleared = true;
         }
     }
 }
