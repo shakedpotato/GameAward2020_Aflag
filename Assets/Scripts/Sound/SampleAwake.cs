2fec30b316ee0e386f162e39f672bf61e98d2eb7
shakedpotato <shakedpotato@gmail.com>
2020-06-09 09:29:46 +0900

    test用の変更

--- Assets/Scripts/Sound/SampleAwake.cs
+++ Assets/Scripts/Sound/SampleAwake.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 public class SampleAwake : MonoBehaviour
 {
-    [SerializeField] private SoundManager soundManager;
-    // Start is called before the first frame update
-    void Start()
-    {
-        soundManager.Load();
-        //BGM開始
-        soundManager.StartBGM(0);
-    }
+    //[SerializeField] private SoundManager soundManager;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    soundManager.Load();
+    //    //BGM開始
+    //    soundManager.StartBGM(0);
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        //BGM変更
-        if(Input.GetKeyDown(KeyCode.Z))
-        {
-            soundManager.OnBGM(1);
-        }
-        //効果音再生
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            soundManager.PlayEffectSound(0);
-        }
-        //BGM止め
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            soundManager.OffBGM();
-        }
-    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    //BGM変更
+    //    if(Input.GetKeyDown(KeyCode.Z))
+    //    {
+    //        soundManager.OnBGM(1);
+    //    }
+    //    //効果音再生
+    //    if (Input.GetKeyDown(KeyCode.X))
+    //    {
+    //        soundManager.PlayEffectSound(0);
+    //    }
+    //    //BGM止め
+    //    if (Input.GetKeyDown(KeyCode.C))
+    //    {
+    //        soundManager.OffBGM();
+    //    }
+    //}
 }
