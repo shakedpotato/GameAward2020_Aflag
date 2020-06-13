01208cfca3b21db7452ce9d5645e81f58405a287
Goto-Tatsu <50894018+Goto-Tatsu@users.noreply.github.com>
2020-06-13 17:01:21 +0900

    SelectStageScene_2更新 202006131700現在までのステージを追加

---
+++ Assets/Scripts/stickMover.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stickMover : MonoBehaviour
+{
+    public float moveSpeed;
+    bool loopBack;
+
+    private void Start()
+    {
+        loopBack = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 座標を取得
+        Vector3 pos = transform.localPosition;
+
+        // 移動量を把握
+        // 外側に膨らんだら
+        if (3.3f < pos.x)
+        {
+            loopBack = false;
+        }
+        else if (pos.x < 0.0f)
+        {
+            loopBack = true;
+        }
+
+        // 座標更新
+        if(!loopBack)
+        {
+            // ゴール方向に移動
+            pos.x -= moveSpeed;
+        }
+        else
+        {
+            // 逆方向に移動
+            pos.x += moveSpeed;
+        }
+
+        transform.localPosition = pos;
+    }
+}
